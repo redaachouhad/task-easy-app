2959c65b8b1bb721260a77cb72ba6c986a1e149a
redaachouhad <redachouhad@gmail.com>
2026-02-03 17:18:40 +0100

    fixing some problems

--- lib/firebase/firebase.ts
+++ lib/firebase/firebase.ts
@@ -6,12 +6,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDVyBZldkMgjPT9mOnjfXFgKgknaKAoA20",
-  authDomain: "taskease-415415.firebaseapp.com",
-  projectId: "taskease-415415",
-  storageBucket: "taskease-415415.appspot.com",
-  messagingSenderId: "431894228668",
-  appId: "1:431894228668:web:966aba3b5ae589aacb2ab9",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
